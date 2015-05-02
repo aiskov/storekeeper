1fa1d713465063e3fdd3e143e5ea9ab35e71c329
Anton Iskov <anton.iskov@seamless.se>
2015-05-02 17:19:23 +0200

    Search duplicates

--- module/duplicates.py
+++ module/duplicates.py
@@ -1,10 +1,34 @@
 import os
-import sys
+from hashlib import sha224
 
 
-def search(dir)
-    for root, subdirs, files in os.walk(dir):
-        print('Dir(%s)' % root)
+def search_duplicates(target):
+    digests = dict()
 
-        for filename in files:
-            print('- File(%s)' % r)
+    for root, dirs, files in os.walk(target):
+        for item in files:
+            path = '%s/%s' % (root, item)
+            hash_digest = _create_digest(path)
+
+            if hash_digest not in digests:
+                digests[hash_digest] = set()
+
+            digests[hash_digest].add(path)
+
+    return [value for key, value in digests.iteritems() if len(value) > 1]
+
+
+def _create_digest(target, buffer_size=50000000):
+    hash_sum = sha224()
+
+    with open(target) as f:
+        while True:
+            data = f.read(buffer_size)
+
+            if not data:
+                break
+
+            hash_sum.update(data)
+
+    return '[%s]->%s' % (os.path.getsize(target), hash_sum.hexdigest())
+
---
+++ storekeeper.py
@@ -0,0 +1,5 @@
+from module.duplicates import search_duplicates
+from pprint import pprint
+
+duplicates = search_duplicates('/Users/admin/VM/vm-lamp')
+pprint(duplicates)
